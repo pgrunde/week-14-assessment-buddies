81bca2660d3007abbdbadecba66034ad8273c50e
Peter Grunde <peter.a.grunde@gmail.com>
2014-09-15 09:46:16 -0600

    test passedish

--- spec/features/patients_spec.rb
+++ spec/features/patients_spec.rb
@@ -11,4 +11,14 @@ feature "Patients" do
     expect(page).to have_content(patient.first_name, patient.last_name)
   end
 
+  scenario 'users can visit the index page, click on a patient and see their prescriptions' do
+    user = create_user
+    patient = create_patient
+    login(user)
+
+    click_on (patient.first_name + " " + patient.last_name)
+    expect(page).to have_content "Some Patient"
+    expect(page).to have_content "Some Patient Patient Prescriptions"
+  end
+
 end
