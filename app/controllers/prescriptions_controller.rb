a52a81488f1c36c73f50b4f2dfe43d6e0c3e188a
Peter Grunde <peter.a.grunde@gmail.com>
2014-09-15 10:51:37 -0600

    we believe in nothing lebowski

--- app/controllers/prescriptions_controller.rb
+++ app/controllers/prescriptions_controller.rb
@@ -3,4 +3,20 @@ class PrescriptionsController < ApplicationController
     @prescription = Prescription.new
   end
 
+  def create
+    @prescription = Prescription.new(allowed_params)
+  end
+
+
+
+
+
+private
+
+  def allowed_params
+    params.require(:prescriptions).permit(:schedule, :dosage, :starts_on, :ends_on, :medication_id)
+
+  end
+
+
 end
